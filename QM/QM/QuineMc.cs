31044f262b1b593bdc399daf2dd879174808b477
Абакиров Абай Ильясович <abaikirov@outlook.com>
2015-12-12 14:42:03 +0600

    First commit

---
+++ QM/QM/QuineMc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QM
+{
+    class QuineMc
+    {
+        private List<int> list;
+        public string Result = "";
+
+        public QuineMc(List<Int32> list)
+        {
+            this.list = list;
+        }
+
+        public List<int> getList()
+        {
+            return list;
+        }
+
+        public void count()
+        {
+
+        }
+    }
+}
---
+++ QM/QM/countForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QM
+{
+    public partial class countForm : Form
+    {
+        public countForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCount_Click(object sender, EventArgs e)
+        {
+            string numbs = textBoxNumbs.Text;
+
+            List<Int32> Numbs = StringToList.Convert(numbs);
+
+            QuineMc quineMc = new QuineMc(Numbs);
+            quineMc.count();
+            richTextBoxResult.Text = quineMc.Result;
+
+        }
+    }
+}
